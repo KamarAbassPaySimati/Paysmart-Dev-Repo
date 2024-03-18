ea2c489246ca1c8f6f4a3496e6af737dfe1b466d
Ranjith N <ranjith.n@7edge.com>
2024-03-18 19:02:56 +0530

    NN-16 #time 10m ci: build number changes

--- scripts/lambda_test.sh
+++ scripts/lambda_test.sh
@@ -7,7 +7,7 @@ APK_FILE="$1"
 response=$(curl -X POST "https://manual-api.lambdatest.com/app/upload/realDevice" \
  -H "accept: application/json" \
  -H "Content-Type: multipart/form-data" \
- -F "name=Paymaart-Customer-$(Build.BuildId)" \
+ -F "name=Paymaart-Customer-$BUILD_NUMBER" \
  -F "visibility=team" \
  -F "appFile=@$APK_FILE"\
  -u "$LT_USERNAME:$LT_ACCESS_KEY"
