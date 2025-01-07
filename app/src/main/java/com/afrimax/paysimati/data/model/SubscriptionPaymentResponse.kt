21def6c512453c04548a056368a0d32ab60d0553
Ajeeb P <ajeeb@7edge.com>
2025-01-07 17:06:07 +0530

    NN-1135 #time 2h bugfix: Modify API mappings.  - Fixed issues with app crashes while loading plans.  - Reorganized the codebase.

--- app/src/main/java/com/afrimax/paysimati/data/model/SubscriptionPaymentResponse.kt
+++ app/src/main/java/com/afrimax/paysimati/data/model/SubscriptionPaymentResponse.kt
@@ -2,17 +2,9 @@ package com.afrimax.paysimati.data.model
 
 import com.google.gson.annotations.SerializedName
 
-//data class SubscriptionPaymentResponse(
-//
-//)
-
 data class SubscriptionPaymentRequestBody(
-    @SerializedName("ref_no")
-    val referenceNumber: String?,
-    @SerializedName("sub_type")
-    val subType: String?,
-    @SerializedName("credential")
-    val credentials: String?,
-    @SerializedName("auto_renew")
-    val autoRenew: Boolean
+    @SerializedName("ref_no") val referenceNumber: String?,
+    @SerializedName("sub_type") val subType: String?,
+    @SerializedName("credential") val credentials: String?,
+    @SerializedName("auto_renew") val autoRenew: Boolean
 )
