dd04baea51a36fbd1629ed8251e260c8afac288b
Ajeeb P <ajeeb@7edge.com>
2025-02-05 16:01:45 +0530

    NN-1186 #time 1h bugfix: Implemented Recaptcha v2 Fixes.
     - Removed the dependency of the Recaptcha client from the `RecaptchaManager` object, now injecting `RecaptchaManager` class.
     - Addressed the potential issue of the Recaptcha client failing to load, by implementing a check for the `recaptchaClient` and continuing if it fails.
     - Fixed Recaptcha in the following screens: Registration, Forgot Password and Verification BottomSheet.
     - Added Toast message after OTP is sent in ForgotPasswordPinActivity.
     - Tested the changes made.

--- app/src/main/java/com/afrimax/paysimati/util/RecaptchaManager.kt
+++ app/src/main/java/com/afrimax/paysimati/util/RecaptchaManager.kt
@@ -4,30 +4,15 @@ import android.app.Application
 import com.afrimax.paysimati.BuildConfig
 import com.google.android.recaptcha.Recaptcha
 import com.google.android.recaptcha.RecaptchaClient
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
 
-object RecaptchaManager {
+class RecaptchaManager(val application: Application) {
 
-    private var recaptchaClient: RecaptchaClient? = null
-    private val job = Job()
-    private val scope = CoroutineScope(Dispatchers.Main + job)
-
-     fun initialise(application: Application) {
-        if (recaptchaClient == null ){
-            scope.launch {
-                Recaptcha.getClient(application, BuildConfig.SITE_KEY)
-                    .onSuccess { client ->
-                        recaptchaClient = client
-                    }
-                    .onFailure { exception ->
-                        "Response".showLogE("Recaptcha Client Error: ${exception.message}")
-                    }
-            }
+    suspend fun getClient(): RecaptchaClient? {
+        Recaptcha.getClient(application, BuildConfig.SITE_KEY).onSuccess { client ->
+            return client
+        }.onFailure { exception ->
+            "Response".showLogE("Recaptcha Client Error: ${exception.message}")
         }
+        return null
     }
-
-    fun getClient(): RecaptchaClient? = recaptchaClient
 }
