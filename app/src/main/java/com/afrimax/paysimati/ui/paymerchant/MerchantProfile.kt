0c0689027fd762d5f965003b54aee804e8a49f9c
Mohammed Shoaib <mohammed.shoaib@7edge.com>
2025-02-07 14:09:08 +0530

    NN-430 #time 1m feature:merchant profile view -logupdate

--- app/src/main/java/com/afrimax/paysimati/ui/paymerchant/MerchantProfile.kt
+++ app/src/main/java/com/afrimax/paysimati/ui/paymerchant/MerchantProfile.kt
@@ -2,7 +2,7 @@ package com.afrimax.paysimati.ui.paymerchant
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+
 import android.view.View
 import android.widget.TextView
 import androidx.core.content.ContextCompat
