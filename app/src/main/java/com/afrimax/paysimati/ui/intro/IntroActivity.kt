bcf2b661f46d0c110e0c651db53ebd7894a225a5
Watipaska Manda <kamar.abass@afrimax.net>
2025-06-23 11:24:25 +0200

    Push more layout and resource files

--- app/src/main/java/com/afrimax/paysimati/ui/intro/IntroActivity.kt
+++ app/src/main/java/com/afrimax/paysimati/ui/intro/IntroActivity.kt
@@ -2,6 +2,7 @@ package com.afrimax.paysimati.ui.intro
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,32 +10,105 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import com.afrimax.paysimati.BuildConfig
 import com.afrimax.paysimati.databinding.ActivityIntroBinding
+import com.afrimax.paysimati.ui.home.HomeActivity
 import com.afrimax.paysimati.ui.login.LoginActivity
-import com.afrimax.paysimati.ui.register.RegisterActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.amazonaws.auth.CognitoCachingCredentialsProvider
+import com.amazonaws.regions.Regions
 
 class IntroActivity : AppCompatActivity() {
     private lateinit var binding: ActivityIntroBinding
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private val RC_SIGN_IN = 1001
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityIntroBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(0, 0, 0, systemBars.bottom)
             insets
         }
+
         val windowInsetsController = WindowInsetsControllerCompat(window, window.decorView)
         windowInsetsController.isAppearanceLightStatusBars = false
-        setContentView(binding.root)
 
-        val version  ="Version ${BuildConfig.VERSION_NAME}"
+        val version = "Version ${BuildConfig.VERSION_NAME}"
         binding.introActivityVersionTV.text = version
 
-        binding.registerButton.setOnClickListener {
-            startActivity(Intent(this, RegisterActivity::class.java))
+        // Google Sign-In config
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // Handle clicks
+        binding.continueWithGoogle.setOnClickListener {
+            val signInIntent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
         }
-        binding.loginButton.setOnClickListener {
+
+        binding.continueWithFacebook.setOnClickListener {
+            // TODO: Facebook login
+        }
+
+        binding.continueWithEmail.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if (account != null) {
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                account?.idToken?.let { idToken ->
+                    federateWithCognito(idToken)
+                }
+            } catch (e: ApiException) {
+                Log.e("GoogleSignIn", "Sign in failed", e)
+            }
+        }
+    }
+
+    private fun federateWithCognito(idToken: String) {
+        val logins = mapOf("accounts.google.com" to idToken)
+        val credentialsProvider = CognitoCachingCredentialsProvider(
+            applicationContext,
+            BuildConfig.CUSTOMER_COGNITO_IDENTITY_POOL_ID,
+            Regions.fromName(BuildConfig.REGION)
+        )
+        credentialsProvider.logins = logins
+
+        Thread {
+            try {
+                credentialsProvider.refresh()
+                Log.d("Cognito", "Federated identity ID: ${credentialsProvider.identityId}")
+                runOnUiThread {
+                    startActivity(Intent(this, HomeActivity::class.java))
+                    finish()
+                }
+            } catch (e: Exception) {
+                Log.e("Cognito", "Federation failed", e)
+            }
+        }.start()
     }
 }
